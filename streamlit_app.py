24b807d8bf6399cc0bb0b2650c962222404b2de4
Selullian <selullian8@gmail.com>
2025-08-19 06:10:46 +0000

    01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Pleasure to be meeting you, sir. Quite a pleasure!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is one of my owner's first application, but It isn't cool."
 )
